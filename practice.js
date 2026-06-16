e77bad64c822153ce180f2b667056fb5d1222cf9
Yashfa Zahid <yashfa229@gmail.com>
2026-06-16 17:11:42 +0500

    responsiveness + added functionality

--- practice.js
+++ practice.js
@@ -5,6 +5,8 @@ let board = [
 ];
 let player1Symbol;
 let player2Symbol;
+let player_x_score=0;
+let player_o_score=0;
 
 function chooseSymbol(){
     do{
@@ -13,6 +15,8 @@ function chooseSymbol(){
     } while(player1Symbol!=="X"&&player1Symbol!=="O")  
     player2Symbol=(player1Symbol==="O")? "X":"O"
     console.log(player2Symbol)
+
+    
 }
 
 function personTurn(){
@@ -24,42 +28,106 @@ function personTurn(){
 function handleClick(event){
         let row = event.target.dataset.row;
         let col = event.target.dataset.col;
+
         if (board[row][col] !== "") return;
+
         board[row][col] = currentPlayer;
+
         if (event.target.innerText===""){
             console.log("clicked")
         event.target.innerText=currentPlayer
+        console.log("done clicking")
+}
         let win=winner();
+
         if(win!==undefined){
-            alert(win+" has won the game!")
+            console.log(currentPlayer)
+            if(currentPlayer==="X"){
+                player_x_score++;
+                let score=document.getElementById("score-x")
+                score.innerText=player_x_score
+                console.log("score updated")
+            }
+            else{
+                player_o_score++;
+                let score=document.getElementById("score-o")
+                score.innerText=player_o_score
+            }
+            const winner_array = winner();
+
+            console.log(winner_array)
+            let btns=[];
+            for (let i=0;i<3;i++){
+                btns.push(document.querySelector(`[data-row="${winner_array[i][0]}"][data-col="${winner_array[i][1]}"]`))
+            }
+            btns.forEach(function(b){
+                b.style.backgroundColor="#00885d"
+                b.style.color="white"
+            })
+            setTimeout(function(){
+                resetgame();
+            },2000)
+        }
+
+        else if(tie()){
+            alert("The game has been tied!")
+            resetgame();
         }
         currentPlayer=(currentPlayer==player1Symbol)? player2Symbol:player1Symbol;
+        turnBoardText.innerText=`Player ${currentPlayer} Turn`;
         console.log(board)
     }
-}
+
 
 
 function winner(){
     //rows
     for (let i=0;i<3;i++){
         if(board[i][0]!=""&&board[i][0]==board[i][1] && board[i][1]==board[i][2] ){
-           return board[i][0]
+           return [[i, 0], [i, 1], [i, 2]];
         }
         //cols
         else if(board[0][i]!=""&&board[0][i]==board[1][i] && board [1][i]==board[2][i]){ 
-            return board[0][i];
+            return [[0,i],[1,i],[2,i]]
         }
 
 }
 //diagonals
         if(board[0][0]!=""&&board[0][0]==board[1][1]&&board[1][1]==board[2][2]){
-        return board[0][0];
+        return [[0,0],[1,1],[2,2]]
         }
         else if(board[0][2]!=""&&board[0][2]==board[1][1]&&board[1][1]==board[2][0]){
-        return board[0][2];
+        return [[0,2],[1,1],[2,0]]
     }
-    }
+}
 
+function tie(){
+    for (let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            if(board[i][j]==""){
+                return false
+            }
+            
+        }
+    }
+    return true;
+}
+
+function resetgame(){
+    let gameButtons=document.querySelectorAll(".game-btns")
+    gameButtons.forEach(function(btn){
+        btn.innerText="";
+        btn.style.backgroundColor="white"
+        btn.style.color="black"
+    })
+    for (let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            board[i][j]=""
+        }
+    }
+}
 chooseSymbol();
 let currentPlayer=player1Symbol;
+let turnBoardText=document.querySelector(".turn-board-txt")
+turnBoardText.innerText=`Player ${currentPlayer} Turn`
 personTurn();
